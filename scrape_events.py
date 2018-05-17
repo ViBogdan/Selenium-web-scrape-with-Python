b6eef9330c59f6f88e91879d50477652edf58fc7
Gabriel Gradinaru <gabriel.gradinaru@avira.com>
2018-05-17 22:55:57 +0300

    Further impovements to date

--- scrape_events.py
+++ scrape_events.py
@@ -3,6 +3,7 @@ import re
 import time
 
 from dateutil.parser import parse
+from dateutil.tz import tzutc
 from selenium import webdriver
 from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.orm import sessionmaker
@@ -45,36 +46,39 @@ for fb_page in config.fb_pages:
     driver.maximize_window()
     time.sleep(3)
 
-    my_element = driver.find_element_by_xpath("//*[contains(text(), 'Past Events') or contains(text(), 'Past events')]")
-    driver.execute_script("return arguments[0].scrollIntoView();", my_element)
+    past_events = driver.find_element_by_xpath("//*[contains(text(), 'Past Events') or contains(text(), 'Past events')]")
+    driver.execute_script("return arguments[0].scrollIntoView();", past_events)
     time.sleep(5)
 
     elements_containers = driver.find_elements_by_class_name('_24er') #identifies the paragraph containing the each event
     for element_container in elements_containers:
 
-        d = datetime.date.today()
-        now = datetime.datetime.combine(d, datetime.datetime.min.time())
+        # Create a datetime for today at midnight with timezone info
+        now = datetime.datetime.now()
+        current_day_start = now.replace(
+            tzinfo=tzutc(), hour=0, minute=0, second=0, microsecond=0
+        )
 
         month = element_container.find_element_by_class_name('_5a4-').text
         day = element_container.find_element_by_class_name('_5a4z').text
-        hour_support = element_container.find_element_by_css_selector('._4dml.fsm.fwn.fcg').text
+        raw_time_str = element_container.find_element_by_css_selector('._4dml.fsm.fwn.fcg').text
 
-        # TODO add UTC timezone to parsed_date
         # TODO handle multiple event days
         # Example date strings:
         # Sat 19:00 UTC+03 · 34 guests
         # Wed 7:00 PM UTC+03
-        # 3 May–4 May · 1,582 guests
-        hour = re.search('(\d{1,2}:\d{2}\s+(?:AM|PM\s+)?)UTC\+\d{2}', hour_support)
+        # 3 May–4 May · 1,582 guests (not handled)
+        match = re.search('(\d{1,2}:\d{2}\s+(?:AM|PM\s+)?)UTC(\+\d{2})', raw_time_str)
 
-        if not hour:
-            print("Failed to find hour from '{}'".format(hour_support))
+        if not match:
+            print("Failed to find match from '{}'".format(raw_time_str))
             break
 
-        datetime_string = '{} {} {}'.format(month, day, hour.group(1))
+        datetime_string = ' '.join([month, day, match.group(1), match.group(2)])
         parsed_date = parse(datetime_string)
 
-        if parsed_date < now:
+        print(parsed_date, raw_time_str)
+        if parsed_date < current_day_start:
             break
 
         New_Event = Event(
