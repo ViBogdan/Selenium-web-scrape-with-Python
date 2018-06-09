7f1b6efac0646462921e33b746e18b022c101fbd
Bogdan <bogdan.i.vintila@gmail.com>
2018-06-10 01:16:02 +0300

    Added a text area which gets populated with checkbox selections. Working on redirecting to text-area after pressing submit

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import render_template
+from flask import render_template, request, redirect,url_for
 from manifest_cultural import app, db
 from manifest_cultural.models import Event
 
 
-@app.route("/")
-@app.route("/index")
+@app.route("/", methods=["POST", "GET"])
 def index():
     events = db.session.query(Event)
-    return render_template('index.html', events=events)
+    new_events = []
+
+    if request.method == "POST":
+        new_events = request.form.getlist('new_events')
+        # return redirect(url_for('index', _anchor="services", new_events=new_events))
+
+    return render_template('index.html', events=events, new_events=new_events)
 
 
 if __name__ == "__main__":
