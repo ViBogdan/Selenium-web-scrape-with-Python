f66a75c0cfe8124d835744d89c69c9d0516b4608
Gabriel Gradinaru <gabriel.gradinaru.gt@gmail.com>
2018-06-10 20:45:47 +0300

    Make smooth scrolling without reloading page

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-from flask import render_template, request
+from flask import render_template
 from manifest_cultural import app, db
 from manifest_cultural.models import Event
 
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/")
 def index():
     events = db.session.query(Event)
-    new_events = []
-
-    if request.method == "POST":
-        new_events = request.form.getlist('new_events')
-
-    return render_template('index.html', events=events, new_events=new_events)
+    return render_template('index.html', events=events)
 
 
 if __name__ == "__main__":
