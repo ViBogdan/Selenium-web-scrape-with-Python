dc3021a6fdd2fc2e2115eb32fc463255f97c7cec
Gabriel Gradinaru <gabriel.gradinaru.gt@gmail.com>
2018-06-23 15:04:07 +0300

    Do not refresh page for chechboxes and filters

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 import datetime
 
-from flask import render_template
+from flask import render_template, jsonify, request
 from sqlalchemy import func, or_
 
 from manifest_cultural import app, db
 from manifest_cultural.models import Event
 
 
-@app.route("/", defaults={'when': None})
-@app.route("/<when>")
-def index(when):
+@app.route("/")
+def index():
+    events = db.session.query(Event)
+    return render_template('index.html', events=events)
+
+
+@app.route('/ajax/events')
+def get_events():
+    when = request.args.get('when')
     if when == 'today':
         today = datetime.date.today()
         events = db.session.query(Event).filter(
             or_(
                 func.DATE(Event.start_date) == today,
+                func.DATE(Event.end_date) == today,
                 func.DATE(Event.first_date) == today,
                 func.DATE(Event.second_date) == today,
                 )
@@ -24,6 +31,7 @@ def index(when):
         events = db.session.query(Event).filter(
             or_(
                 func.DATE(Event.start_date) == tomorrow,
+                func.DATE(Event.end_date) == tomorrow,
                 func.DATE(Event.first_date) == tomorrow,
                 func.DATE(Event.second_date) == tomorrow,
             )
@@ -31,7 +39,9 @@ def index(when):
     else:
         events = db.session.query(Event)
 
-    return render_template('index.html', events=events)
+    # serialize events as dict to be able to json encode them
+    events = [event.as_dict() for event in events]
+    return jsonify(events)
 
 
 if __name__ == "__main__":
